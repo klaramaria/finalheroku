62a452b28e4bb974a8d6e1b343b23a3d7f33b84d
klaramaria <klarawesselkamp@gmail.com>
2022-09-20 08:44:50 +0200

    fixed bugs

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from dash import Dash, dash_table, html, dcc 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import pandas as pd
 import json
 from employee_keys import extra_column, string_example
@@ -9,13 +9,22 @@ from setting_keys import extra_column_s
 #------------------ function to convert unix time to readable time ----------------------
 def clean_time(time):
     if(time):
-        if len(time) == 26: #time with timezone localization
-            ntime = time[6:-7]
-            ntime = int(ntime) + (3.6e+6)*2 # convert to MET
-            ntime = pd.to_datetime(ntime, unit = 'ms').strftime("%d/%m/%y %H:%M")
-        if len(time) == 21: #without timezone localization
-            ntime = time[6:-2]
-            ntime = pd.to_datetime(ntime, unit = 'ms').strftime("%d/%m/%y %H:%M")
+        try: 
+            if len(time) == 26: #time with timezone localization
+                ntime = time[6:-7]
+                ntime = int(ntime) + (3.6e+6)*2 # convert to MET
+                ntime = pd.to_datetime(ntime, unit = 'ms').strftime("%d/%m/%y %H:%M")
+            if len(time) == 21: #without timezone localization
+                print(2)
+                ntime = time[6:-2]
+                ntime = pd.to_datetime(ntime, unit = 'ms').strftime("%d/%m/%y %H:%M")
+            if len(time) == 20:
+                print(3)
+                ntime = time[6:-2]
+                ntime = pd.to_datetime(ntime, unit = 'ms').strftime("%d/%m/%y %H:%M")
+        except: 
+            ntime = 'Time could not be deciphered. Double check the formatting. Otherwise contact Klara about this'
+            
     else: 
         ntime = time
     return ntime
@@ -27,6 +36,7 @@ def make_table_from_json(json_object, key):
     #key_list.remove('Name')
     if key == 'Employees':
         df = pd.DataFrame(json_object[key])
+        
     else: 
         df = pd.DataFrame(json_object, index = [0])
     time_column_names = ['DepartmentStartDate', 'StartDate', 'EndDate', 'ContractTypeStartDate', 'ContractHourStartDate', 'PositionStartDate']
@@ -38,9 +48,6 @@ def make_table_from_json(json_object, key):
     df = df.T.reset_index().rename(columns = {0: 'Value', 'index':'Key'})
     df['Explanation / Alternate Name'] = df.Key.map(extra_column).fillna('-')
     df = df.astype(str)
-    if 'CustomColumns' in df['Key'].values:
-        df[df['Key']=='CustomColumns']['Value'].values[0]= df[df['Key']=='CustomColumns']['Value'].values[0].replace('},','},\n')
-        print(df[df['Key']=='CustomColumns']['Value'].values[0])
     return df
 
 #----------------- functions that transforms settings part of json object into pandas dataframe ----------
@@ -55,7 +62,6 @@ server = app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Make your files more readable'),
-    dcc.Dropdown(options = ['JSON', 'XML'], value = 'JSON', id='dropdown'),
     dcc.Textarea(
         id='textarea-example',
         value=string_example,
@@ -63,6 +69,8 @@ app.layout = html.Div(children=[
     ),
     ## div in which we get told if object is correct or not correct
     html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line', 'marginBottom': 25, 'marginTop': 25}),
+
+    html.Button('Submit', id = 'submit-val'),
 
     html.H3(id='Employees_header', children = ''),
 
@@ -92,36 +100,37 @@ app.layout = html.Div(children=[
 @app.callback(
     Output('textarea-example-output', 'children'),
     Output('textarea-example', 'value'),
-    Input('dropdown', 'value'),
-    Input('textarea-example', 'value')
+    State('textarea-example', 'value'),
+    Input('submit-val','n_clicks')
 )
-def update_output(ddval, txtvalue):
-    if ddval == 'JSON':
+def update_info(txtvalue, n_clicks):
+    if n_clicks != 0: 
         try: 
             json_object = json.loads(txtvalue)
             return 'This is a correct JSON object \n', txtvalue
         except:
             json_object = "none"
             return 'This is not a json object', string_example
-    else: 
-        return 'We are working on the XML Option', 'Paste your XML-Object in here. Nothing will happen but you can practice your pasting.'
+
 
 
 # ------ update data_table on callback ----------------
 @app.callback(
     [Output("data_table", "data"), Output('data_table', 'columns'),
     Output("settings_table", "data"), Output('settings_table', 'columns')],
-    Input('dropdown','value'),
-    Input('textarea-example', 'value')
+    State('textarea-example', 'value'),
+    Input('submit-val','n_clicks')
 )
-def update_output(ddval, value):
-    if ddval == 'JSON':
+def update_output(value, n_clicks):
+    if n_clicks != 0:
         try:
             json_object = json.loads(value)
             if 'Employees' in list(json_object.keys()): 
                 df1 = make_table_from_json(json_object, 'Employees')
                 df2 = display_settings(json_object)
                 df2 = df2.astype(str)
+                print(df1)
+                print(df2)
                 return df1.to_dict('records'), [{"name": i, "id": i} for i in df1.columns], df2.to_dict('records'), [{"name": i, "id": i} for i in df2.columns]
             else:
                 try:
@@ -132,8 +141,7 @@ def update_output(ddval, value):
                     return [], [] , [] , []
         except: 
             return [], [] , [] ,[]
-    else:
-        return [], [], [], []
+
 
 # ----- update settings_header and Employees_header on callback------------------------- 
 @app.callback(
